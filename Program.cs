0c1a4f260b62c98aa0b7971f5f2647cb7dc7c674
Owain Williams <owain@hedfan.uk>
2023-11-16 23:14:23 +0000

    Add .PNG as a file format

--- Program.cs
+++ Program.cs
@@ -13,12 +13,23 @@ if (args.Length != 2)
 var filename = args[1];
 var extension = Path.GetExtension(filename);
 
-if (!extension.Equals(".svg", StringComparison.CurrentCultureIgnoreCase))
+var validExtensions = new[] { ".SVG", ".PNG" };
+
+if (!validExtensions.Contains(extension.ToUpper()))
 {
     Console.WriteLine($"Invalid file extension {extension}");
     return;
 }
 
 var qr = QrCode.EncodeText(args[0], QrCode.Ecc.Medium);
-string svg = qr.ToSvgString(4);
-File.WriteAllText(filename, svg, Encoding.UTF8);
+
+switch (extension.ToUpper())
+{
+    case ".PNG":
+        qr.SaveAsPng(filename, scale: 10, border: 4);
+        break;
+    default:
+        string svg = qr.ToSvgString(4);
+        File.WriteAllText(filename, svg, Encoding.UTF8);
+        break;
+}
