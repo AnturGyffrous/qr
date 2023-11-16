83e4f5fb12f811dc18e6931feb9c0ff8aa752a75
Owain Williams <owain@hedfan.uk>
2023-11-16 21:07:00 +0000

    Output usage details if an unknown number of args are passed in

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@
 
 using Net.Codecrete.QrCodeGenerator;
 
+if (args.Length != 2)
+{
+    Console.WriteLine("Usage: qr text filename");
+    return;
+}
+
 var qr = QrCode.EncodeText(args[0], QrCode.Ecc.Medium);
 string svg = qr.ToSvgString(4);
 File.WriteAllText(args[1], svg, Encoding.UTF8);
